c5d29bf7894fad1caabfdb81c5fc8171301c6eb8
V C Ashwin <vcashwin99@gmail.com>
2017-08-14 17:43:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,6 +14,11 @@ app.get('/counter',function(req,res){
    counter=counter+1;
    res.send(counter.toString());
 });
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
