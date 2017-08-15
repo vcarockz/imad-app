e6fe75e319640ad285bfa519a2afca5cadc3fa2a
V C Ashwin <vcashwin99@gmail.com>
2017-08-15 14:31:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,18 @@
-console.log('Loaded!');
+var button= document.getElementById('button');
 
-var counter=0;
-var button=document.getElementById('button');
-button.onclick= function(){
-    var span=document.getElementById('count');
-    counter=counter+1;
-    span.innerHTML=counter.toString();
-    
-}
+button.onclick = function(){
+  var request = new XMLhttpRequest();
+  
+  request.onreadystatechange = function(){
+    if(request.readyState === XMLhttpRequest.DONE){
+        if(request.status === 200) {
+            var counter = reqeust.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML= counter.toString();
+        }
+    }  
+  };
+  
+  request.open('GET','http://http://vcashwin99.imad.hasura-app.io/counter', true);
+  request.send(null);
+};
