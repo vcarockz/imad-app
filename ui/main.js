416259980ce07b675607ce173a2bb7591e400a50
V C Ashwin <vcashwin99@gmail.com>
2017-08-15 14:34:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,6 +13,6 @@ button.onclick = function(){
     }  
   };
   
-  request.open('GET','http://http://vcashwin99.imad.hasura-app.io/counter', true);
+  request.open('GET','http://vcashwin99.imad.hasura-app.io/counter', true);
   request.send(null);
 };
